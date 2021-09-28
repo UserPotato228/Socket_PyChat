57c839fce5d0d33518cde6f1d09ff0fae65f12e5
YourPersonalZhigul <49975241+UserPotato228@users.noreply.github.com>
2021-09-28 22:17:59 +0300

    Update server.py

--- server.py
+++ server.py
@@ -4,13 +4,13 @@ IP = "localhost"
 PORT = 8080
 clients = [] 
 server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server.bind((IP, PORT)) 
-server.listen(15)
+server.bind((IP, PORT))
 while True:
-   data, addr = server.recvfrom(2048) 
-   if addr not in clients:
-      clients.append(addr)
-   for client in clients:
-      if client in clients:
-         continue
-   server.sendto(data, client) 
+	data, addr = server.recvfrom(2048)
+	print("[",addr,"] ["+data.decode("utf8")+"]")
+	if addr not in clients:
+		clients.append(addr)
+	for client in clients:
+		if client == addr:
+			continue
+		server.sendto(data, client)
